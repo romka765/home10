29d7c77f38b269b5770e87398af76c617d2170d6
romka765 <yatskevichroman@gmail.com>
2018-01-03 19:13:33 +0300

    Initial commit

---
+++ src/main/java/controllers/MoveController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MoveController {
+    private int mass [][];
+    private String playerCount;
+    private String compCount;
+
+
+
+    public String startGame(HttpServletRequest){
+       return "1";
+    }
+
+
+    public doMove(HttpServletRequest request){
+        request.getParameter("name");
+    }
+}
